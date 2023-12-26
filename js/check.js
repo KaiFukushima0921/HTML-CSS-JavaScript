1697c3e658cd47b630e29fba918f363f0d24116f
KaiFukushima0921 <kai2co2@gmail.com>
2023-12-26 19:26:29 +0900

    JavaScript-Chapter4完了

---
+++ js/check.js
@@ -0,0 +1,6 @@
+const isAgree = document.querySelector('#check');
+const btn = document.querySelector('#btn');
+
+isAgree.addEventListener('change', () => {
+    btn.disabled = !isAgree.checked;
+});
